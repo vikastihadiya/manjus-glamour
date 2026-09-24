8a0cbba21779218e0818ff51ac55dd0d9d8ab444
vikastihadiya <vikastihadiya@gmail.com>
2026-09-24 19:54:51 +0530

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,11 +1,6 @@
-// ============================================================
-// MANJU'S THE WORLD OF GLAMOUR
-// Supabase Configuration
-// ============================================================
-
 const SUPABASE_URL = "https://xrxpvjxqdqxybyohxrjy.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_WDjegJoZs6zagx7jiPn6qR_a60EdHtp";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_WDjegJoZs6zagx7jiPn6rQ_a60EdHtp";
 
 const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
